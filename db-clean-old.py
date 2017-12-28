8d1154ffea45699da6436eb9ed42afa4cda681aa
Andy <a.magnusson@gmail.com>
2017-12-28 10:12:35 -0800

    Added another old file (db-clean-old.py)

---
+++ db-clean-old.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Cleans up the database by deleting untouched records older than
+# a certain date. By default this script assumes anything older than 28
+# days is stale and can be removed.
+# v0.1
+# Andrew Magnusson
+
+from pymongo import MongoClient
+import datetime, sys
+
+# globals
+# Mongo connection parameters
+client = MongoClient('mongodb://localhost:27017')
+db = client['vulnmgt']
+
+# Number of days after which data is considered stale
+olderThan = 28
+
+# print usage and exit
+def usage():
+    print ('''
+Usage: $ db-clean.py
+
+    ''')
+
+def main():
+
+    # get the current date to determine how old is too old
+    date = datetime.datetime.utcnow()
+    oldDate = date - datetime.timedelta(days=olderThan)
+
+    # now delete all records older than that date.
+    # do this to hosts and hostvuln.
+
+    hostsremoved = db.hosts.find({'updated': {'$lt': oldDate}}).count()
+    vulnremoved = db.hostvuln.find({'updated': {'$lt': oldDate}}).count()
+    #db.hosts.remove({'updated': {'$lt': oldDate}})
+    #db.hostvuln.remove({'updated': {'$lt': oldDate}})
+
+    print("Stale hosts removed:", hostsremoved, "\nStale host-vuln mappings removed:", vulnremoved)
+
+main()
